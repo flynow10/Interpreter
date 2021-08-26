fb19a53f9d305e269e705c1ed9e34d67063112c8
Nate Wagner <nate.o.wagner@gmail.com>
2021-08-25 21:50:22 -0400

    Fixed bugs and compiled to binary

--- out/compile.bat
+++ out/compile.bat
@@ -1,4 +1,5 @@
+cd C:/Code/java/Interpreter/out/
 rmdir .\classes\ /s /q
 mkdir .\classes\
-javac -d ..\out\classes\ ..\src\com\wagologies\Interpreter.java ..\src\com\wagologies\Lexer.java ..\src\com\wagologies\Parser.java ..\src\com\wagologies\Scope.java
+javac -d ..\out\classes\ ..\src\com\wagologies\*.java ..\src\com\wagologies\Parser\*.java ..\src\com\wagologies\Parser\Nodes\*.java
 javapackager.exe -createjar -appclass com.wagologies.Interpreter -srcdir classes -outfile outjar -v
